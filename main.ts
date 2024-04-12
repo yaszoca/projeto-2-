68a3403e6fad2738ae611e8a1aa01ea18c7851ea
yaszoca <166840480+yaszoca@users.noreply.github.com>
2024-04-12 13:28:32 -0300

    Arquivos iniciais para o projeto MakeCode

---
+++ main.ts
@@ -0,0 +1,15 @@
+basic.forever(function () {
+	
+})
+basic.forever(function () {
+    basic.showIcon(IconNames.Duck)
+    basic.pause(500)
+    basic.showLeds(`
+        . . . . .
+        . # # . .
+        # # # . .
+        . # # # #
+        . # # # .
+        `)
+    basic.pause(500)
+})
